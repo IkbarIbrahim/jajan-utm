cc9dcb9d699001d7b4c3a9c6c89dde8d004d654a
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-05-14 22:43:02 +0700

    kerangka commit + add bootstap npm

--- resources/views/Frontend/Component/Master.blade.php
+++ resources/views/Frontend/Component/Master.blade.php
@@ -4,21 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="w-5">
-        <button
-            type="button"
-            name=""
-            id=""
-            class="btn btn-primary"
-        >
-            Button
-        </button>
-    </div>
-    
-    
+    @yield('header')
+    @yield('content')
+    @yield('footer')
 </body>
 </html>
