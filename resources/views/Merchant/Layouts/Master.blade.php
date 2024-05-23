96be95d6881e7c7741ae68a7403b963812f85a1a
Shafy Gunawan <shafygunawan@gmail.com>
2024-05-23 23:32:22 +0700

    add merchant - product edit page

--- resources/views/Merchant/Layouts/Master.blade.php
+++ resources/views/Merchant/Layouts/Master.blade.php
@@ -1,77 +1,82 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title')</title>
-  <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-  <link rel="stylesheet" href="assets/css/style.css">
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    <div class="bg-gray-500">
+    <div class="bg-gray-200 dark:bg-gray-500">
+
         @include('Merchant.Layouts.Sidebar')
+
         @yield('content')
 
         @include('Merchant.Layouts.Footer')
-        
+
     </div>
-      
+
     <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+            var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
-document.addEventListener('DOMContentLoaded', (event) => {
-        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-        // Initial theme check
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark');
-            if (themeToggleLightIcon) themeToggleLightIcon.classList.remove('hidden');
-            if (themeToggleDarkIcon) themeToggleDarkIcon.classList.add('hidden');
-        } else {
-            document.documentElement.classList.remove('dark');
-            if (themeToggleLightIcon) themeToggleLightIcon.classList.add('hidden');
-            if (themeToggleDarkIcon) themeToggleDarkIcon.classList.remove('hidden');
-        }
-
-        // Theme toggle button event listener
-        var themeToggleBtn = document.getElementById('theme-toggle');
-        themeToggleBtn.addEventListener('click', function() {
-            themeToggleDarkIcon.classList.toggle('hidden');
-            themeToggleLightIcon.classList.toggle('hidden');
-
-            if (localStorage.getItem('color-theme')) {
-                if (localStorage.getItem('color-theme') === 'light') {
-                    document.documentElement.classList.add('dark');
-                    localStorage.setItem('color-theme', 'dark');
-                } else {
-                    document.documentElement.classList.remove('dark');
-                    localStorage.setItem('color-theme', 'light');
-                }
+            // Initial theme check
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window
+                    .matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+                if (themeToggleLightIcon) themeToggleLightIcon.classList.remove('hidden');
+                if (themeToggleDarkIcon) themeToggleDarkIcon.classList.add('hidden');
             } else {
-                if (document.documentElement.classList.contains('dark')) {
-                    document.documentElement.classList.remove('dark');
-                    localStorage.setItem('color-theme', 'light');
-                } else {
-                    document.documentElement.classList.add('dark');
-                    localStorage.setItem('color-theme', 'dark');
-                }
+                document.documentElement.classList.remove('dark');
+                if (themeToggleLightIcon) themeToggleLightIcon.classList.add('hidden');
+                if (themeToggleDarkIcon) themeToggleDarkIcon.classList.remove('hidden');
             }
+
+            // Theme toggle button event listener
+            var themeToggleBtn = document.getElementById('theme-toggle');
+            themeToggleBtn.addEventListener('click', function() {
+                themeToggleDarkIcon.classList.toggle('hidden');
+                themeToggleLightIcon.classList.toggle('hidden');
+
+                if (localStorage.getItem('color-theme')) {
+                    if (localStorage.getItem('color-theme') === 'light') {
+                        document.documentElement.classList.add('dark');
+                        localStorage.setItem('color-theme', 'dark');
+                    } else {
+                        document.documentElement.classList.remove('dark');
+                        localStorage.setItem('color-theme', 'light');
+                    }
+                } else {
+                    if (document.documentElement.classList.contains('dark')) {
+                        document.documentElement.classList.remove('dark');
+                        localStorage.setItem('color-theme', 'light');
+                    } else {
+                        document.documentElement.classList.add('dark');
+                        localStorage.setItem('color-theme', 'dark');
+                    }
+                }
+            });
         });
-    });
-      
-      function onToggleMenu(e){
-      e.name = e.name === 'menu' ? 'close' : 'menu' 
-      }  
-      const collapse = new Collapse($targetEl, $triggerEl, options, instanceOptions);
-  
-      $triggerEl.addEventListener('click', function () {
-          collapse.toggle();
-      });
+
+        function onToggleMenu(e) {
+            e.name = e.name === 'menu' ? 'close' : 'menu'
+        }
+        const collapse = new Collapse($targetEl, $triggerEl, options, instanceOptions);
+
+        $triggerEl.addEventListener('click', function() {
+            collapse.toggle();
+        });
     </script>
-    
+
     <script src="https://unpkg.com/@popperjs/core@2"></script>
 </body>
+
 </html>
