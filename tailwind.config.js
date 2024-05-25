f2d66f01a0a25dfe17969180580797378d69c2f4
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-05-25 12:33:29 +0700

    update admin product & admin chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,44 @@ export default {
     "node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'body': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ],
+        'sans': [
+      'Inter', 
+      'ui-sans-serif', 
+      'system-ui', 
+      '-apple-system', 
+      'system-ui', 
+      'Segoe UI', 
+      'Roboto', 
+      'Helvetica Neue', 
+      'Arial', 
+      'Noto Sans', 
+      'sans-serif', 
+      'Apple Color Emoji', 
+      'Segoe UI Emoji', 
+      'Segoe UI Symbol', 
+      'Noto Color Emoji'
+    ]
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
