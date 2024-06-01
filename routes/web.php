2fa021eb9ba10c0c46b0b05db344d8e30b754835
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-06-01 10:21:18 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\MerchantController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormRegisterController;
+use App\Http\Controllers\Merchant\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,7 +46,6 @@ Route::get('/postingan', function () {
 Route::get('/login', function () {
     return view('Auth.Login');
 })->name('login');
-
 Route::get('/global', function () {
     return view('Pages.Global-chat');
 })->name('global');
@@ -56,15 +56,13 @@ Route::prefix('merchant')
         Route::get('/', function () {
             return view('Merchant.Index');
         })->name('index');
+
         Route::prefix('products')
             ->name('products.')
             ->group(function () {
-                Route::get('/', function () {
-                    return view('Merchant.Products.Index');
-                })->name('index');
-                Route::get('/{product}/edit', function () {
-                    return view('Merchant.Products.Edit');
-                })->name('edit');
+                Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('edit');
+                Route::put('/{product}', [ProductController::class, 'update'])->name('update');
+                Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
             });
     });
 
@@ -107,5 +105,3 @@ Route::prefix('admin')
                 })->name('index');
             });
     });
-
-
