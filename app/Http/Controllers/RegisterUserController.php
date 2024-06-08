199258616eb7d60d7f1c7a041508e3729f8d168b
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-06-08 15:02:49 +0700

    update

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -8,7 +8,8 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class RegisterUserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('Auth.register-form.form-user');
     }
 
@@ -17,20 +18,17 @@ class RegisterUserController extends Controller
         $request->validate([
             'first-name' => 'required',
             'last-name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
         ], [
-            'first-name.required' => 'nama awal wajib diisi',
-            'last-name.required' => 'nama akhir wajib diisi',
+            'first-name.required' => 'Nama awal wajib diisi',
+            'last-name.required' => 'Nama akhir wajib diisi',
+            'email.required' => 'Email wajib diisi',
             'email.email' => 'Format email tidak valid',
+            'email.unique' => 'Email sudah terdaftar',
             'password.required' => 'Password wajib diisi',
+            'password.min' => 'Password minimal 8 karakter',
         ]);
-
-        $existingUser = User::where('email', $request->input('email'))->first();
-        if ($existingUser) {
-            Alert::info('User', 'Email anda sudah terdaftar');
-            return redirect()->back()->withInput();
-        }
 
         // Create new user
         $user = new User();
@@ -40,7 +38,7 @@ class RegisterUserController extends Controller
         $user->password = bcrypt($request->input('password'));
         $user->save();
 
-        // Redirect ke halaman tertentu setelah berhasil menyimpan data
+        // Redirect to a specific page after successful registration
         Alert::info('User', 'Anda telah registrasi');
         return redirect()->route('login')->with('success', 'Registrasi berhasil!');
     }
