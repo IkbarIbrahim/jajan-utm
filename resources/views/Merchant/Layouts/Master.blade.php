e54ec38297098e0d656d117cbc28ec7cd5d704c8
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-06-26 11:12:32 +0700

    fixing merchant bug

--- resources/views/Merchant/Layouts/Master.blade.php
+++ resources/views/Merchant/Layouts/Master.blade.php
@@ -12,8 +12,10 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     {{-- <link rel="stylesheet" href="assets/css/style.css"> --}}
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+  
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @yield('head')
 </head>
 
 <body>
