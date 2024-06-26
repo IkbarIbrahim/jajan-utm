a9991cbb682f758df5d0daa27b85566fbacc1e09
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2024-06-26 11:05:06 +0700

    update versi leaflet

--- resources/views/Merchant/Layouts/Master.blade.php
+++ resources/views/Merchant/Layouts/Master.blade.php
@@ -12,11 +12,8 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     {{-- <link rel="stylesheet" href="assets/css/style.css"> --}}
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    @yield('head')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </head>
 
 <body>
